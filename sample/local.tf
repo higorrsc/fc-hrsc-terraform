7ce1166e2fe0114b0f77efb485bf039deb98a77a
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-07-28 19:19:47 -0300

    feat: :sparkles: add datasource to terraform

--- sample/local.tf
+++ sample/local.tf
@@ -3,6 +3,16 @@ resource "local_file" "exemplo" {
   content  = var.content
 }
 
+data "local_file" "example-content" {
+  filename = "example.txt"
+}
+
+output "data-source-result" {
+  value = data.local_file.example-content.content
+  # Uncomment the line below to get the content in base64 format
+  # value = data.local_file.example-content.content_base64
+}
+
 variable "content" {
   type = string
 }
